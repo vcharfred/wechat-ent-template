b84d1d8881ba688ce0f57b1ad14387f21692ab6d
vcharfred <2515513925@qq.com>
2022-07-10 16:09:23 +0800

    feat: init project

---
+++ src/main/java/top/vchar/wechat/WechatApplication.java
@@ -0,0 +1,19 @@
+package top.vchar.wechat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * <p> 启动类 </p>
+ *
+ * @author vchar fred
+ * @version 1.0
+ * @create_date 2022/7/10
+ */
+@SpringBootApplication
+public class WechatApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WechatApplication.class, args);
+    }
+}
